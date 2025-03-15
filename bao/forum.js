db9d945e03aa00bd47f7ea11959fd6f8c359919e
noobcoder123456789 <noobcoder1234567@gmail.com>
2025-03-15 21:13:48 +0700

    update forum (noobcoder)

---
+++ bao/forum.js
@@ -0,0 +1,13 @@
+function loadComponent(id, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        })
+        .catch(error => console.error(`Lỗi khi tải ${file}:`, error));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadComponent("header", "../component/header.html");
+    loadComponent("footer", "../component/footer.html");
+});
