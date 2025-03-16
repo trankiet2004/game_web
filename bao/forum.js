eda8347d7c4ba6f36ec5164dcf12e3b2c33addb7
noobcoder123456789 <noobcoder1234567@gmail.com>
2025-03-16 08:57:52 +0700

    update forum (noobcoder)

--- bao/forum.js
+++ bao/forum.js
@@ -10,4 +10,18 @@ function loadComponent(id, file) {
 document.addEventListener("DOMContentLoaded", function () {
     loadComponent("header", "../component/header.html");
     loadComponent("footer", "../component/footer.html");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, {
+        threshold: 0.1 
+    });
+
+    document.querySelectorAll('[data-scroll]').forEach((element) => {
+        observer.observe(element);
+    });
 });
