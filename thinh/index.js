d7c4b479195f788155d33ced61d17c9859c22ef3
QuocThinh73 <ttqthinh2004@gmail.com>
2025-03-16 14:26:50 +0700

    change color theme index & add contact page

---
+++ thinh/index.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('.slider_3d .item img').forEach(img => {
+    img.addEventListener('click', function(){
+      const clickedSrc = this.getAttribute('src');
+      
+      const modelDiv = document.querySelector('.banner .content .model');
+      
+      let modelBg = window.getComputedStyle(modelDiv).backgroundImage;
+      if(modelBg.startsWith('url(')){
+          modelBg = modelBg.slice(4, -1).replace(/"/g, "");
+      }
+      
+      modelDiv.style.backgroundImage = `url(${clickedSrc})`;
+      this.setAttribute('src', modelBg);
+    });
+});
+
+const navEl = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 56) {
+        navEl.classList.add('navbar-scrolled')
+    } else if (window.scrollY < 56) {
+        navEl.classList.remove('navbar-scrolled')
+    }
+});
