023baf9b929e5d3b7e982f06bc60f73deb2932b5
noobcoder123456789 <noobcoder1234567@gmail.com>
2025-04-16 11:40:16 +0700

    feat: implement POST functionality for FAQ submission and enhance form handling in forum.html

--- API/controllers/FetchController.php
+++ API/controllers/FetchController.php
@@ -37,10 +37,39 @@ class FetchController {
         }
     }
 
-    public function fetch($method, $table, $id, $idColumn = "id") {
+    public function POST($table, $json) {
+        if($table === "faqs") {
+            $question = $json[0];
+            $answer = $json[1];
+            $posted_by = $json[2];
+            $stmt = $this->connect->prepare("INSERT INTO faqs (question, answer, posted_by) VALUES (?, ?, ?)");
+            
+            if($stmt === false) {
+                http_response_code(500);
+                echo json_encode(["success" => false, "message" => "Prepare statement thất bại: " . $this->connect->error]);
+                exit;
+            }
+
+            $stmt->bind_param("sss", $question, $answer, $posted_by);
+            if ($stmt->execute()) {
+                echo json_encode(["success" => true, "message" => "Câu hỏi đã được cập nhật thành công"]);
+            } else {
+                http_response_code(500);
+                echo json_encode(["success" => false, "message" => "Có lỗi xảy ra: " . $stmt->error]);
+            }
+
+            $stmt->close();
+        }
+    }
+
+    public function fetch($method, $table, $id, $idColumn = "id", $json = NULL) {
         switch ($method) {
             case "GET":
                 $this->GET($table, $id, $idColumn);
+                break;
+
+            case "POST":
+                $this->POST($table, $json);
                 break;
         }
     }
--- API/faqs.php
+++ API/faqs.php
@@ -8,4 +8,18 @@ spl_autoload_register(function($class) {
 $URIPart = explode("/", $_SERVER["REQUEST_URI"]);
 $fetch = new FetchController($connect);
 $id = substr($URIPart[count($URIPart) - 1], strlen($URIPart[count($URIPart) - 1]) - 3) === "php" ? null : $URIPart[count($URIPart) - 1];
-$fetch->fetch($_SERVER["REQUEST_METHOD"], "faqs", $id, "faq_id");
+if($_SERVER["REQUEST_METHOD"] === "POST" && !(isset($_POST["questionTitle"]) && isset($_POST["questionContent"]))) {
+    var_dump(isset($_POST["questionTitle"]));
+    var_dump(isset($_POST["questionContent"]));
+    http_response_code(400);
+    echo json_encode(["success" => false, "message" => "Thiếu dữ liệu yêu cầu"]);
+    exit;
+}
+
+$json = $_SERVER["REQUEST_METHOD"] === "POST" ? Array(
+    trim($_POST["questionTitle"]),
+    trim($_POST["questionContent"]),
+    "Admin"
+) : NULL;
+
+$fetch->fetch($_SERVER["REQUEST_METHOD"], "faqs", $id, "faq_id", $json);
