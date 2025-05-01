1185a17f47866ebce42b3e7943e98e2987696307
noobcoder123456789 <noobcoder1234567@gmail.com>
2025-05-01 10:35:50 +0700

    Refactor authentication pages to use .php extension and update links accordingly

--- View/common_part/signin.php
+++ View/common_part/signin.php
@@ -12,25 +12,25 @@ unset($_SESSION['err'], $_SESSION['old']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng Nhập</title>
-    <link rel="shortcut icon" href="/game_web/assets/compiled/svg/favicon.svg" type="image/x-icon">
-    <link rel="icon" type="image/icon" href="/View/img/logo.png">
-    <link rel="stylesheet" href="/View/assets/compiled/css/app.css">
-    <link rel="stylesheet" href="/View/assets/compiled/css/app-dark.css">
-    <link rel="stylesheet" href="/View/assets/compiled/css/auth.css">
+    <link rel="shortcut icon" href="../assets/compiled/svg/favicon.svg" type="image/x-icon">
+    <link rel="icon" type="image/icon" href="../img/logo.png">
+    <link rel="stylesheet" href="../assets/compiled/css/app.css">
+    <link rel="stylesheet" href="../assets/compiled/css/app-dark.css">
+    <link rel="stylesheet" href="../assets/compiled/css/auth.css">
     <style>
         body { font-family: system-ui, -apple-system, 'Segoe UI', Roboto, 'Helvetica Neue', 'Noto Sans', Arial, sans-serif !important; }
     </style>
 </head>
 
 <body>
-    <script src="/View/assets/static/js/initTheme.js"></script>
+    <script src="../assets/static/js/initTheme.js"></script>
 
     <div id="auth">
         <div class="row h-100">
             <div class="col-lg-5 col-12" style="background-color: #051209 !important;">
                 <div id="auth-left">
                     <div class="auth-logo d-flex justify-content-center">
-                        <a href="../thinh/index.html"><img src="/View/img/logo.png" class="rounded-circle" style="transform:scale(4.5)" alt="Logo"></a>
+                        <a href="../thinh/index.html"><img src="../img/logo.png" class="rounded-circle" style="transform:scale(4.5)" alt="Logo"></a>
                     </div>
                     <h1 class="auth-title">Đăng Nhập</h1>
 
@@ -67,8 +67,8 @@ unset($_SESSION['err'], $_SESSION['old']);
                     </div>
 
                     <div class="text-center mt-5 text-lg fs-4">
-                        <p class="text-gray-600">Bạn chưa có tài khoản? <a href="signup.html" class="font-bold text-success">Đăng Ký</a>.</p>
-                        <p><a class="font-bold text-success" href="forgot-password.html">Quên Mật Khẩu?</a>.</p>
+                        <p class="text-gray-600">Bạn chưa có tài khoản? <a href="signup.php" class="font-bold text-success">Đăng Ký</a>.</p>
+                        <p><a class="font-bold text-success" href="forgot-password.php">Quên Mật Khẩu?</a>.</p>
                     </div>
                 </div>
             </div>
