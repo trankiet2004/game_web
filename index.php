19f3b02bdd10ed950b0fb4b857d00269d1e44adf
kaktuzzzzz <tu.nguyenlevan2004@hcmut.edu.vn>
2025-05-04 19:59:04 +0700

    headerr

--- index.php
+++ index.php
@@ -5,103 +5,93 @@ require_once('Controller/PlatformsController.php');
 require_once('Controller/GenresController.php');
 require_once('Controller/TagsController.php');
 require_once('Controller/DevelopersController.php');
-
-// Create controller objects
-$controllers = [
-    'game' => new GamesController(),
-    'platform' => new PlatformsController(),
-    'genre' => new GenresController(),
-    'tag' => new TagsController(),
-    'developer' => new DevelopersController()
-];
+// Create controller object
+$gamecontroller = new GamesController();
+$platformcontroller = new PlatformsController();
+$genrescontroller = new GenresController();
+$tagcontroller = new TagsController();
+$devcontroller = new DevelopersController();
 
 $page = $_GET['page'] ?? '';
 $action = $_GET['action'] ?? null;
 $id = $_GET['id'] ?? null;
 
-// Helper function to include views
-function includeView($viewPath) {
-    include($viewPath);
-}
-
 // Handle AJAX request
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action'])) {
-    $postActions = [
-        'load_products' => 'load_products',
-        'load_platforms' => 'load_platforms',
-        'load_genres' => 'load_genres',
-        'load_tags' => 'load_tags',
-        'load_developers' => 'load_developers',
-        'load_more_games' => 'load_more_games',
-        'load_more_games_in_genres' => 'load_more_games',
-        'load_more_games_in_tags' => 'load_more_games',
-        'load_more_games_in_developers' => 'load_more_games'
-    ];
-
-    $action = $_POST['action'];
-    if (isset($postActions[$action])) {
-        $controllerKey = explode('_', $action)[1];  // e.g., 'platform' from 'load_platforms'
-        if (isset($controllers[$controllerKey])) {
-            $controllers[$controllerKey]->{$postActions[$action]}();
-        }
-        exit; // End the script after processing the AJAX request
-    }
-}
-
-// Route actions to appropriate controllers
-if ($action && $id !== null) {
-    $actionMap = [
-        'game' => 'get_game_by_id',
-        'platform' => 'get_platform_by_id',
-        'genres' => 'get_genres_by_id',
-        'tag' => 'get_tag_by_id',
-        'developer' => 'get_developer_by_id'
-    ];
-
-    if (array_key_exists($action, $actionMap)) {
-        $controllers[$action]->{$actionMap[$action]}($id);
-    }
-} elseif ($action) {
-    // Default action is to show the index page for each controller
-    $indexActions = [
-        'game' => 'index',
-        'platform' => 'index',
-        'genres' => 'index',
-        'tag' => 'index',
-        'developer' => 'index'
-    ];
-
-    if (array_key_exists($action, $indexActions)) {
-        $controllers[$action]->{$indexActions[$action]}();
-    }
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'load_products') {
+    $gamecontroller->load_products();
+} else if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'load_platforms'){
+    $platformcontroller->load_platforms();
+} else if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'load_genres'){
+    $genrescontroller->load_genres();
+} else if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'load_tags'){
+    $tagcontroller->load_tags();
+} else if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'load_developers'){
+    $devcontroller->load_developers();
+}  else if($action === 'load_more_games'){
+    $platformcontroller->load_more_games();
+} else if($action === 'load_more_games_in_genres'){
+    $genrescontroller->load_more_games();
+} else if($action === 'load_more_games_in_tags'){
+    $tagcontroller->load_more_games();
+} else if($action === 'load_more_games_in_developers'){
+    $devcontroller->load_more_games();
+} else if($action === 'game' && $id !== null){
+    $gamecontroller->get_game_by_id($id);
+} else if($action === 'game'){
+    $gamecontroller->index();
+} else if($action === 'platform' && $id !== null){
+    $platformcontroller->get_platform_by_id($id);
+} else if($action === 'platform'){
+    $platformcontroller->index();
+}else if($action === 'genres' && $id !== null){
+    $genrescontroller->get_genres_by_id($id);
+} else if($action === 'genres'){
+    $genrescontroller->index();
+} else if($action === 'tag' && $id !== null){
+    $tagcontroller->get_tag_by_id($id);
+} else if($action === 'tag'){
+    $tagcontroller->index();
+} else if($action === 'developer' && $id !== null){
+    $devcontroller->get_developer_by_id($id);
+} else if($action === 'developer'){
+    $devcontroller->index();
+} else if($page == 'tu_game') {
+    include('./View/admin/bao_comment.php');
+} else if($page == 'contact_us') {
+    include('./View/thinh/contact_us.php');
+} else if($page == 'about_us') {
+    include('./View/bao/about_us.php');
+} else if($page == 'blogs') {
+    include('./View/kiet/blogs.php');
+} else if($page == 'detail') {
+    include('./View/kiet/detail.php');
+} else if($page == 'forum') {
+    include('./View/bao/forum.php');
+} else if($page == 'signin') {
+    include('./View/common_part/signin.php');
+} else if($page == 'signup') {
+    include('./View/common_part/signup.php');
+} else if($page == 'forgot-password') {
+    include('./View/common_part/forgot-password.php');
+} else if($page == 'account-profile') {
+    include('./View/common_part/account-profile.php');
+} else if($page == 'logout') {
+    include('./View/common_part/logout.php');
+} else if($page == 'comment_manage') {
+    include('./View/common_part/comment_manage.php');
+} else if($page == 'indexAdmin') {
+    include('./View/admin/index.php');
+} else if($page == 'bao_comment') {
+    include('./View/admin/bao_comment.php');
+} else if($page == 'bao_page_manage') {
+    include('./View/admin/bao_page_manage.php');
+} else if($page == 'bao_edit_page') {
+    include('./View/admin/bao_edit_page.php');
+} else if($page == 'kiet_blog_manage') {
+    include('./View/admin/kiet_blog_manage.php');
+} else if($page == 'user-management') {
+    include('./View/admin/user-management.php');
 } else {
-    // Page Routing
-    $pageViews = [
-        'tu_game' => './View/admin/bao_comment.php',
-        'contact_us' => './View/thinh/contact_us.php',
-        'about_us' => './View/bao/about_us.php',
-        'blogs' => './View/kiet/blogs.php',
-        'detail' => './View/kiet/detail.php',
-        'forum' => './View/bao/forum.php',
-        'signin' => './View/common_part/signin.php',
-        'signup' => './View/common_part/signup.php',
-        'forgot-password' => './View/common_part/forgot-password.php',
-        'account-profile' => './View/common_part/account-profile.php',
-        'logout' => './View/common_part/logout.php',
-        'comment_manage' => './View/common_part/comment_manage.php',
-        'indexAdmin' => './View/admin/index.php',
-        'bao_comment' => './View/admin/bao_comment.php',
-        'bao_page_manage' => './View/admin/bao_page_manage.php',
-        'bao_edit_page' => './View/admin/bao_edit_page.php',
-        'kiet_blog_manage' => './View/admin/kiet_blog_manage.php',
-        'user-management' => './View/admin/user-management.php'
-    ];
-
-    if (isset($pageViews[$page])) {
-        includeView($pageViews[$page]);
-    } else {
-        // Default view for unknown pages
-        includeView('./View/thinh/index.php');
-    }
+    // Default to the index (product list)
+    include('./View/thinh/index.php');
 }
-?>
